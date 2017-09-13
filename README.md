f3d0c893b969eeae5b96674dcac3c1379fb71f64
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2017-09-13 16:05:44 -0300

    aula 3

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curso Udacity: Programa Nanodegree React
+
+Aplicação ContatctList
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+class App extends Component {
+
+  state = {
+    screen: 'list', // list, create
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+    ContactsAPI.remove(contact)
+  }
+
+  navigateToCreate = () => {
+    this.setState({...this.state, screen: 'create'})
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        {this.state.screen === 'list' && (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+            onNavigate={this.navigateToCreate}
+          />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
